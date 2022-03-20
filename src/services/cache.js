ba89adf281f0d8bd87fe9633a5dc80983f936143
Bela <ufuomaogodo@gmail.com>
2022-03-20 13:41:56 +0100

    add changes

--- src/services/cache.js
+++ src/services/cache.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const { redisURL } = require('../../config');
-const client = redis.createClient(redisURL);
+const client = redis.createClient({ url: redisURL });
 
 (async () => {
   await client.connect();
